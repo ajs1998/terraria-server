c9e199eb77b7d96148ddb3d8b7261b741037f05f
Alex Sweeney <ajs1998@gmail.com>
2022-07-24 06:25:25 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Paper server
+
+Run `./start.sh` to start the server.
+The volume will be attached at `./data`.
---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+sudo docker run -it --rm \
+	-p 7777:7777 \
+	-v $(pwd)/data:/root/.local/share/Terraria/Worlds:Z \
+	ryshe/terraria:vanilla-latest \
+	-world /root/.local/share/Terraria/Worlds/World.wld \
+	-autocreate 1
